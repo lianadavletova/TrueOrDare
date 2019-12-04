3ce8eed30d21f308bd26e1c4b99be034418bf6ca
lianadavletova <46841020+lianadavletova@users.noreply.github.com>
2019-12-04 21:43:46 +0500

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace ПравдаИлиДействие
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        //string connectionString;
+        SqlDataAdapter adapter;
+        bool newGame;
+
+        public Window1()
+        {
+            InitializeComponent();
+            //connectionString = "Data Source=ADMN-ПК;Initial Catalog=truth_Or_Desire;Integrated Security=True";
+            newGame = false;
+            connect1.Open();
+            SqlCommand cmd = connect1.CreateCommand();
+            cmd.CommandText = "delete from Players";
+            cmd.ExecuteNonQuery();
+            connect1.Close();
+        }
+
+        public void UpdateListBox(SqlConnection con, ListView listBox1)
+        {
+            listBox1.Items.Clear();
+            con.Open();
+            SqlCommand command = new SqlCommand();
+            command.Connection = con;
+            string query = "SELECT name_p FROM Players";
+            command.CommandText = query;
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["name_p"].ToString());
+            }
+            con.Close();
+        }
+        SqlConnection connect1 = new SqlConnection(@"Data Source=ADMN-ПК;Initial Catalog=truth_Or_Desire;Integrated Security=True");
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            connect1.Open();
+            SqlCommand cmd = connect1.CreateCommand();
+            // cmd.CommandType = CommandType.Text;
+            if (newGame == false)
+            {
+                cmd.CommandText = "insert into Players values(1,'" + namePlayer.Text + "')";
+                newGame = true;
+            }
+            else
+            {
+                cmd.CommandText = "insert into Players values((select max(id_p) from Players)+1,'" + namePlayer.Text + "')";
+            }
+            cmd.ExecuteNonQuery();
+            connect1.Close();
+            UpdateListBox(connect1, listPlayers);
+        }
+        //int count;
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            //count = Convert.ToInt32(listPlayers.Items.Count);
+            Window4 mw = new Window4();
+            this.Close();
+            mw.Show();
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            this.Close();
+            mw.Show();
+        }
+    }
+}
---
+++ Window4.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace ПравдаИлиДействие
+{
+    /// <summary>
+    /// Логика взаимодействия для Window4.xaml
+    /// </summary>
+    public partial class Window4 : Window
+    {
+        Random rnd;
+        SqlConnection connect1 = new SqlConnection(@"Data Source=ADMN-ПК;Initial Catalog=truth_Or_Desire;Integrated Security=True");
+        int[] arr;
+
+        public int[] RandomArr(Random rnd, int lenght)
+        {
+            int[]arr = new int[lenght];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var _r = rnd.Next(1, lenght);
+                if (!(arr.Contains(_r)))
+                {
+                    arr[i] = _r;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            return arr;
+        }
+        int countarr;
+        public Window4()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            connect1.Open();
+            SqlCommand cmd = connect1.CreateCommand();
+            cmd.CommandText = "select count(id_p) as count from Players";
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                countarr = Convert.ToInt32(reader["count"]);
+            }
+            connect1.Close();
+            arr = RandomArr(rnd, countarr);
+            connect1.Open();
+            SqlCommand cmd1 = connect1.CreateCommand();
+            cmd1.CommandText = "select id_p from Players where id_p = '"+arr[0]+"'";
+            SqlDataReader reader1 = cmd1.ExecuteReader();
+            while (reader1.Read())
+            {
+                nameplayer.Content = reader1["name_p"].ToString();
+            }
+            connect1.Close();
+        }
+    }
+}
